b94f46232a509373c26155f051dfc5fc7c043845
NeroXie <xyh30902@163.com>
2024-09-12 11:30:18 +0800

    update

--- src/types.ts
+++ src/types.ts
@@ -1,23 +1,23 @@
-export interface Double {};
-export interface Float {};
-export interface Integer {};
-export interface PromiseCb {};
-export interface SuccessCb {};
-export interface FailureCb {};
+export interface Double {}
+export interface Float {}
+export interface Integer {}
+export interface PromiseCb {}
+export interface SuccessCb {}
+export interface FailureCb {}
 
 /** 插件类型 */
 export enum PluginType {
     Module = 'rn_module', // RN 原生模块
     UIComponent = 'rn_ui_component', // RN 原生UI组件
     NULL = 'null', // 用来占位，没有意义
-};
+}
 
 /** 适用平台 */
 export enum Platforms {
     iOS = 'ios',
     Android = 'android',
     All = 'all'
-};
+}
 
 /** UI事件类型 */
 export enum UIEventType {
